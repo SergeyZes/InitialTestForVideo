2f1766a60f06638c86c3b7fa4110923ebd785be6
Zes <sergey_the_great@mail.ru>
2018-05-05 20:06:19 +0500

    Добавлен цикл for.

--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Super code goes here...
+        for (int i=0;i<10; i++){
+           //do something cool here...
+        }
     }
 }
